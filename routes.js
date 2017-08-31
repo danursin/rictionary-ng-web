22febf50b95ef0619a05c4bcf21616dc4b1800eb
danursin <danursin@gmail.com>
2017-08-30 21:22:18 -0500

    routees

--- routes.js
+++ routes.js
@@ -3,9 +3,16 @@ const path = require("path");
 exports.routes = [
     {
         method: 'GET',
+        path: '/src/{param*}',
+        handler: (request, reply) => {
+            reply.file(path.join(__dirname, request.params.param));
+        }
+    },
+    {
+        method: 'GET',
         path: '/dist/{param*}',
         handler: (request, reply) => {
-            reply.file(path.join(__dirname,, "/dist/", request.params.param));
+            reply.file(path.join(__dirname, "/dist/", request.params.param));
         }
     },
     {
