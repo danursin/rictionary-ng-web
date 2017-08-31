665cc009583c55afaa499239d4d4d0c6a866b384
danursin <danursin@gmail.com>
2017-08-30 21:29:20 -0500

    squeak

--- routes.js
+++ routes.js
@@ -11,8 +11,11 @@ exports.routes = [
     {
         method: 'GET',
         path: '/dist/{param*}',
-        handler: (request, reply) => {
-            reply.file(path.join(__dirname, "/dist/", request.params.param));
+        handler: {
+            directory: {
+                path: path.join(__dirname, 'dist'),
+                listing: true
+            }
         }
     },
     {
